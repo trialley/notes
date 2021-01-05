d39b9442a154ad1be0e0e0c2fd88c416ecb24521
TriAlley <1203732857@qq.com>
2021-01-05 17:13:19 +0800

    nav move

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,7 +1,8 @@
 const head = require('../../config/head.js');
 const plugins = require('../../config/plugins.js');
-const themeConfig = require('../../config/themeConfig.js');
-
+themeConfig = require('../../config/themeConfig.js');
+const nav =  require('./nav.js');
+themeConfig.nav = nav;
 module.exports = {
   theme: 'vdoing', // 使用依赖包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
