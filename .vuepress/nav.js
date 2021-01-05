104fd415ffc5bc8b54231a2ad3a5ffbe892948f2
TriAlley <trialley@163.com>
2021-01-05 17:43:11 +0800

    about

--- .vuepress/nav.js
+++ .vuepress/nav.js
@@ -1,9 +1,6 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  {
-    text: 'Kernel',
-    link: '/Kernel/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
-  },
-
+  {text: 'Kernel', link: '/Kernel/'},
+  {text: 'About', link: '/about/'},
 ]
