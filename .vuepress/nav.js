0baaa4943659850221458018d1b3cd9212ec5f51
TriAlley <1203732857@qq.com>
2021-01-05 17:25:33 +0800

    nav done

--- .vuepress/nav.js
+++ .vuepress/nav.js
@@ -3,7 +3,7 @@ module.exports = [
   { text: '首页', link: '/' },
   {
     text: 'Kernel',
-    link: '/Kernel', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    link: '/Kernel/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
   },
 
 ]
--- 01.Kernel/Kernel.md
+++ 01.Kernel/Kernel.md
@@ -7,4 +7,5 @@ pageComponent:
     description: 分享Kernel相关经验与学习笔记
 title: Kernel
 permalink: /Kernel
+date: 2021-01-05 17:06:52
 ---
