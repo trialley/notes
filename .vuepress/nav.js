fa17c280cb9e38e9bedfec68f82321df5c6d405c
TriAlley <trialley@163.com>
2021-01-05 18:42:23 +0800

    archives

--- .vuepress/nav.js
+++ .vuepress/nav.js
@@ -6,4 +6,13 @@ module.exports = [
   {text: '云原生', link: '/CloudNative/'},
   {text: '算法', link: '/algorithm/'},
   {text: '关于', link: '/about/'},
+  {
+    text: '索引',
+    link: '/archives/',
+    items: [
+      { text: '分类', link: '/categories/' },
+      { text: '标签', link: '/tags/' },
+      { text: '归档', link: '/archives/' },
+    ],
+  },
 ]
