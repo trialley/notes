7f1813896cedd4a647f1cfa2da939fc52a4d27d4
TriAlley <1203732857@qq.com>
2021-01-14 11:14:56 +0800

    add language

--- 30.云原生/00.Golang/Golang语言学习.md
+++ 30.云原生/00.Golang/Golang语言学习.md
@@ -1,3 +1,13 @@
+---
+title: Golang语言学习
+date: 2021-01-14 10:43:21
+permalink: /pages/a85697/
+categories:
+  - 云原生
+  - Golang
+tags:
+  - 
+---
 
 
 
--- nav.js
+++ nav.js
@@ -2,9 +2,9 @@
 module.exports = [
   { text: '首页', link: '/' },
   {text: '内核与虚拟化', link: '/Kernel/'},
-  {text: 'C/C++', link: '/C/'},
   {text: '云原生', link: '/CloudNative/'},
   {text: '算法', link: '/algorithm/'},
+  {text: '语言特性', link: '/language/'},
   {text: '折腾', link: '/Geek/'},
   {text: '基础', link: '/base/',
     items: [
